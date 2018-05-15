cd02a947a53f3864bf53eaa8f084b312e52f280c
csheng91 <csheng00@gmail.com>
2018-05-14 20:12:06 -0400

    changed promise to callback due to need to execute on error

--- backend/api/methods/newLink.js
+++ backend/api/methods/newLink.js
@@ -6,14 +6,29 @@ const newLink = {newLink: (req, res) => {
   const { url } = req.body;
 
   Link({url: url})
-    .save()
-    .then(savedLink => {
-      res.json(savedLink);
-    })
-    .catch(error => {
-      console.log(error);
-      res.json(error);
-    })
+    .save((error, savedLink) => {
+      if (error && error.code === 11000) {
+        Link.findOne({url: url})
+          .then(foundLink => {
+            res.json(foundLink);
+          })
+          .catch(error2 => {
+            console.log(error2);
+          })
+      } else if (error) {
+        console.log(error);
+        res.json(error);
+      } else {
+        res.json(savedLink);
+      }
+    });
+    // .then(savedLink => {
+    //   res.json(savedLink);
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    //   res.json(error);
+    // })
 }}
 
 module.exports = newLink;
